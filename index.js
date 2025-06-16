5637997d0638fd4cdd74fe687f0bc7605e86abd5
more-devs <moresitoes@gmail.com>
2025-06-16 00:27:39 -0400

    2docomit

--- index.js
+++ index.js
@@ -4,7 +4,8 @@ const db = mysql.createConnection({
     host: process.env.DB_HOST,
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME
+    database: process.env.DB_NAME,
+    database: process.env.DB_PORT
 });
 
 const PORT = process.env.PORT || 3306;
